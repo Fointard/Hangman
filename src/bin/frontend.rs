3d0bcde13e59c99f3d6524f4cde42702a6e421ba
fointard <cmarpaud@gmail.com>
2022-04-30 14:11:48 +0200

    Move pkg desc & author from clap to Cargo.toml

--- src/bin/frontend.rs
+++ src/bin/frontend.rs
@@ -2,11 +2,7 @@ use anyhow::{Context, Result};
 use clap::Parser;
 
 #[derive(Parser)]
-#[clap(
-	author = "Fointard",
-	version = "0.1.0",
-	about = "A simple hangman game written in Rust."
-)]
+#[clap(author, version, about)]
 struct Args {
 	#[clap(short, long, default_value = "library.txt")]
 	library: String,
